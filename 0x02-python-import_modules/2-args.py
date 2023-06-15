99bee005954decfd148464203ec3c18cfc1524ea
Oumaelba <elbachirioumaima16@gmail.com>
2023-06-15 17:45:13 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    argument_count = len(sys.argv) - 1
+
+    if argument_count == 0:
+        print("0 arguments.")
+    elif argument_count == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(argument_count))
+
+    for i in range(argument_count):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+
+if __name__ == '__main__':
+    print_arguments()
