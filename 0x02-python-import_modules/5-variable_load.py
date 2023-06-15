4ef9ce6a75b5628aa8ae65a25f116a394351cbbe
Oumaelba <elbachirioumaima16@gmail.com>
2023-06-15 21:07:33 +0100

    all

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import importlib
+
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    module_name = "variable_load_5"
-    variable_load_5 = importlib.import_module(module_name)
-
-    print(variable_load_5.a)
+    print(a)
