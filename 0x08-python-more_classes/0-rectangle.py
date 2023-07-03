0eba1904d8a1e42deaf462efbc77393ef846be86
Oumaelba <elbachirioumaima16@gmail.com>
2023-07-03 12:02:18 +0100

    try

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Rectangle class.
+"""
+
+
+class Rectangle:
+
+    """
+    This class represents a rectangle.
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+0x08-python-more_classes
+This repository contains Python code examples and exercises related to advanced concepts and features of classes in Python.
