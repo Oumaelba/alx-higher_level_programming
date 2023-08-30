05dd36cb6f307d8836f78918813b04b9a2bed8d5
Oumaelba <elbachirioumaima16@gmail.com>
2023-08-30 21:53:22 +0100

    wholw

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    np = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            np += 1
+        except IndexError:
+            break
+    print("")
+    return np
