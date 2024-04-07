ad1e7e81aa9274bdf6cc175ea436fac6954381f5
Oumaelba <elbachirioumaima16@gmail.com>
2024-04-07 22:37:09 +0000

    task 1done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+This script defines a function add_integer that adds two integers.
+"""
+
+def add_integer(a, b=98):
+	"""
+	The function adds two parameters:
+	
+	Parameters:
+	a: an integer or float
+	b: an integer or float
+
+	Returns:
+	int: The function returns the sum of the two parameters as an integer.
+	"""
+	if isinstance(a, (int,float)):
+		a = int(a)
+	else:
+		raise TypeError("a must be an integer")
+	if isinstance(b, (int, float)):
+		b = int(b)
+	else:
+		raise TypeError("b must be an integer")
+	
+	return a + b
